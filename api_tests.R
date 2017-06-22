77f361a924f62cd9fbc159952a378a95e7aabf7b
Nick Strayer <nick@nicks-mbp.attlocal.net>
2017-06-22 15:37:48 -0500

    updated the scratch script to demo the new functions

--- api_tests.R
+++ api_tests.R
@@ -6,30 +6,11 @@ library(zoo)
 library(scales)
 library(jsonlite)
 
-source("api_helpers.R")
-
-
-fitbit_endpoint <- oauth_endpoint(  authorize = "https://www.fitbit.com/oauth2/authorize",  
-                                    access = "https://api.fitbit.com/oauth2/token")
-
+source("api_helpers.R") #some helpful functions for dealing with the fitbit api.
 source("api_keys.R") #contains a list with strings storing the below values. 
-myapp <- oauth_app( appname = api_keys$appname,
-                    key = api_keys$key,
-                    secret = api_keys$secret)
 
-
-#What the heck, why do I need this?! Pull request will be sent
-Sys.setenv("HTTR_SERVER_PORT" = "1410")
-
-token <- oauth2.0_token(fitbit_endpoint, 
-                        myapp, 
-                        use_basic_auth = TRUE,
-                        scope=c("activity", "heartrate", "sleep"))
-
-conf <- config(token = token)
-
-
-
+# setup a config object for querying api. 
+conf <- make_config(api_keys)
 
 
 #grab data for heart rate at a 1 second granularity for a given day
@@ -41,8 +22,7 @@ my_hr <- get_heart_rate(
   endTime = "23:59"
 )
 
-
-#Plotting heartrate with rolling average
+#Plot it with rolling average
 my_hr %>% 
   mutate(heart_rate_rolling = rollapply(heart_rate,120,mean,align='right',fill=NA)) %>% 
   ggplot(aes(x = time)) + 
@@ -52,7 +32,7 @@ my_hr %>%
 
 
 
-#grab data for steps at a minute level as well
+#grab data for steps at a minute level as well for funs. 
 my_hr_min <- get_heart_rate(
   config = conf, 
   resolution = 'minutes',
